2959fecf00d92d4e33ef591bd186daaebe69e49f
Sourabh <112743236+Sourabh-23@users.noreply.github.com>
2026-06-12 12:31:40 +0530

    Update 20260612064154_create_users_table.js
    migrations

--- src/migrations/20260612064154_create_users_table.js
+++ src/migrations/20260612064154_create_users_table.js
@@ -1,15 +1,16 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('users', function(table) {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('middle_name');
+    table.string('surname').notNullable();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
+    table.enum('status', ['active', 'inactive']).defaultTo('active');
+    table.timestamps(true, true);
+  });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('users');
 };
